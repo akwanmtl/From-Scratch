58ca3bb51efbb84891a1c3d84c87e3a956983d8d
Annie Kwan <akwan.mtl@gmail.com>
2020-11-14 15:33:38 -0500

    add login buttons

---
+++ Assets/JS/login.js
@@ -0,0 +1,6 @@
+var loginBtn = document.getElementById("login");
+var signUpBtn = document.getElementById("sign-up");
+
+loginBtn.addEventListener("click",function(){
+    $("#login-modal").show();
+})
